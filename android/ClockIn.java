7f425843d5e1921c41e0dba4287730c199408701
wumcpq <wumqing@yeah.net>
2020-05-10 19:55:01 +0800

    安卓打卡

---
+++ android/ClockIn.java
@@ -0,0 +1,169 @@
+package com.example.test_apk;
+
+import kotlin.time.Clock;
+import org.jsoup.Connection;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClockIn {
+    String loginUrl;
+    String submitUrl;
+    String pageEncode;
+    String user;
+    String password;
+    String area;
+    String location;
+    String tem;
+    boolean success;
+
+    public boolean getSuccess(){
+        return success;
+    }
+    ClockIn() {
+        try {
+            loginUrl = "https://ssp.scnu.edu.cn/login.aspx";
+            submitUrl = "https://ssp.scnu.edu.cn/";
+            pageEncode = "UTF8";
+
+            user = "";
+            password = "";
+            area = "";
+            location = "";
+            tem = "";
+            success = false;
+        } catch (Exception e) {
+            showInfo(e.getMessage());
+        }
+    }
+
+    /**
+     * 提示信息
+     *
+     * @param info 提示文本
+     */
+    private void showInfo(String info) {
+        //TODO 使用弹窗进行提醒
+        System.out.println(info);
+    }
+
+    /**
+     * 读取网页上的__VIEWSTATE等参数
+     *
+     * @param html   所在页面
+     * @param target 读取参数名
+     * @return 参数值
+     */
+    public String get__(String html, String target) {
+        String res = "";
+
+        Pattern pattern = Pattern.compile("id=\"" + target + "\" value=\"" + "([/+=\\w]+)\"");
+        Matcher matcher = pattern.matcher(html);
+        if (matcher.find()) {
+            res = matcher.group(1);
+        } else {
+            showInfo("找不到" + target);
+        }
+        return res;
+    }
+
+    public boolean clock() {
+        try {
+            String __EVENTVALIDATION;
+            String __VIEWSTATE;
+            Connection login_con = Jsoup.connect(loginUrl);
+            login_con.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36");
+            Response rs = login_con.execute();// 获取响应
+
+            __VIEWSTATE = get__(rs.body(), "__VIEWSTATE");
+            __EVENTVALIDATION = get__(rs.body(), "__EVENTVALIDATION");
+
+            // 获取，cooking和表单属性，下面map存放post时的数据
+            Map<String, String> login_data = new HashMap<>();
+            login_data.put("log_username", user);
+            login_data.put("log_password", password);
+            login_data.put("__EVENTTARGET", "logon");
+            login_data.put("__EVENTVALIDATION", __EVENTVALIDATION);
+            login_data.put("__VIEWSTATE", __VIEWSTATE);
+            // 设置cookie和post上面的map数据
+            Response login = login_con.ignoreContentType(true).method(Method.POST)
+                    .data(login_data).cookies(rs.cookies()).execute();
+
+            // 找key
+            String key = "opt_rc_jkdk.aspx\\?key=[\\w\\d]+&fid=55";
+            Pattern P_key = Pattern.compile(key);
+            Matcher M_key = P_key.matcher(login.body());
+            if (M_key.find()) {
+                key = M_key.group(0);
+
+            } else {
+                showInfo("找不到key！");
+            }
+            submitUrl += key;
+
+            // 转到打卡页面
+            Connection submit_con = Jsoup.connect(submitUrl);
+            submit_con.header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36");
+            Response temp = submit_con.cookies(rs.cookies()).execute();// 获取响应
+            Map<String, String> temp_data = new HashMap<>();
+            temp_data.put("ctl00$cph_right$e_ok", "on");
+            temp_data.put("ctl00$cph_right$ok_submit", "开始填报");
+            temp_data.put("__EVENTVALIDATION", get__(temp.body(), "__EVENTVALIDATION"));
+            temp_data.put("__VIEWSTATE", get__(temp.body(), "__VIEWSTATE"));
+            Response submit = submit_con.ignoreContentType(true).method(Method.POST)
+                    .data(temp_data).cookies(rs.cookies()).execute();
+
+            // 开始打卡
+            Map<String, String> clockin_data = new HashMap<>();
+            clockin_data.put("ctl00$cph_right$e_area", area);
+            clockin_data.put("ctl00$cph_right$e_location", location);
+            clockin_data.put("ctl00$cph_right$e_observation", "无下列情况");
+            clockin_data.put("ctl00$cph_right$e_health$0", "无不适");
+            clockin_data.put("ctl00$cph_right$e_temp", tem);
+            clockin_data.put("ctl00$cph_right$e_survey01", "疫情期间未出国出境");
+            clockin_data.put("ctl00$cph_right$e_submit", "提交保存");
+            clockin_data.put("__EVENTVALIDATION", get__(submit.body(), "__EVENTVALIDATION"));
+            clockin_data.put("__VIEWSTATE", get__(submit.body(), "__VIEWSTATE"));
+            Response clockin = null;
+            Connection final_con = Jsoup.connect(submitUrl);
+            clockin = final_con.ignoreContentType(true).method(Method.POST)
+                    .data(clockin_data).cookies(rs.cookies()).execute();
+            success = clockin.body().contains("打卡成功");
+            System.out.println("打卡："+success);
+            return success;
+        } catch (Exception e) {
+            success = false;
+            return success;
+        }
+    }
+
+    public void start() {
+        clock();
+    }
+
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setTem(String tem) {
+        this.tem = tem;
+    }
+}
---
+++ android/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.example.test_apk
+
+import android.os.Bundle
+import android.os.Looper
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+import java.io.File
+import java.io.FileNotFoundException
+import kotlin.math.tan
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+    var filePath = ""
+
+
+    /**
+     * 保存打卡配置
+     */
+    fun saveData(data: String) {
+
+        try {
+            File(filePath).writeText(data)//覆盖原先的文本内容
+
+        } catch (e: Exception) {
+            //把异常放在文本框中
+            findViewById<EditText>(R.id.tem_text).setText(e.toString())
+        }
+
+    }
+
+    /**
+     * 读取打卡配置
+     */
+    fun loadData() {
+        try {
+            val s = File(filePath).readLines()
+            //读取打卡内容
+            findViewById<EditText>(R.id.user_text).setText(s[0])
+            findViewById<EditText>(R.id.pass_text).setText(s[1])
+            findViewById<EditText>(R.id.area_text).setText(s[2])
+            findViewById<EditText>(R.id.location_text).setText(s[3])
+            findViewById<EditText>(R.id.tem_text).setText(s[4])
+            if (s[5] == "1"){
+                findViewById<CheckBox>(R.id.tem_checkbox).setChecked(true)
+            }
+
+        } catch (e: FileNotFoundException) {
+            //文件未找到即是第一次使用
+            val dialog = AlertDialog.Builder(this).create()
+            dialog.setTitle("帮助")
+            dialog.setMessage("欢迎使用。根据输入框提示输入相应内容，点击打卡按钮即可进行打卡。点击打卡后请耐心等候1至3秒即可。\n" +
+                    "每次打卡都会保存本次打卡内容，下次就不用手动输入直接愉快打卡啦！")
+            dialog.show()
+        }catch (e:Exception){
+
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        filePath = getExternalFilesDir(null)?.absolutePath + File.separator + "clockIn.dat"     // 打卡内容存储路径
+
+        loadData()      //读取上一次配置
+
+        // 按钮点击事件
+        findViewById<Button>(R.id.submit_button).setOnClickListener {
+            // 打卡内容
+            val user = findViewById<EditText>(R.id.user_text).text.toString()
+            val password = findViewById<EditText>(R.id.pass_text).text.toString()
+            val area = findViewById<EditText>(R.id.area_text).text.toString()
+            val location = findViewById<EditText>(R.id.location_text).text.toString()
+            var tem = findViewById<EditText>(R.id.tem_text).text.toString()
+            var rand_tem = "0"
+            if (findViewById<CheckBox>(R.id.tem_checkbox).isChecked){
+                //随机额温
+                rand_tem = "1"
+                tem = (360..365).random().div(10.0).toString()
+                findViewById<EditText>(R.id.tem_text).setText(tem)
+            }
+            // 存储本次打卡配置
+            val data = user + "\n" + password + "\n" + area + "\n" + location + "\n" + tem + "\n" + rand_tem
+
+            //进行打卡
+            Thread {
+                Looper.prepare()
+                val ci = ClockIn()
+                // 设置打卡参数
+                ci.setUser(user)
+                ci.setPassword(password)
+                ci.setArea(area)
+                ci.setLocation(location)
+                ci.setTem(tem)
+                ci.start()
+                print("kt 打卡:"+ci.getSuccess())
+                val dialog = AlertDialog.Builder(this).create()
+                if(ci.getSuccess()){
+                    saveData(data)
+                    dialog.setMessage("打卡成功！打卡额温为：$tem")
+                }else{
+                    dialog.setMessage("打卡失败！请检查账号密码或者打卡内容！")
+                }
+                dialog.show()
+                Looper.loop()
+            }.start()
+        }
+
+    }
+}
